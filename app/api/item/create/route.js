7b180d8e6d4756c61c68449fe0bcd36452f2baf0
usxc <tinypopopo@gmail.com>
2025-10-16 12:22:16 +0900

    POSTメソッドでアイテム作成処理にリクエストボディを渡すように変更

--- app/api/item/create/route.js
+++ app/api/item/create/route.js
@@ -3,13 +3,13 @@ import connectDB from "@/app/utils/database";
 import { ItemModel } from "@/app/utils/schemaModels";
 
 export async function POST(request) {
-    
+
     const reqBody = await request.json()
     console.log(reqBody)
 
     try{
         await connectDB()
-        await ItemModel.create()
+        await ItemModel.create(reqBody)
         return NextResponse.json({message: "アイテム作成成功"}) 
     }catch{
         return NextResponse.json({message: "アイテム作成失敗"}) 
