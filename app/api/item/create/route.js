6be4557b5213f806ca51c4003b9d0d0d18db824b
usxc <tinypopopo@gmail.com>
2025-10-16 10:45:49 +0900

    データベースに接続できているかのテストコードを実装

--- app/api/item/create/route.js
+++ app/api/item/create/route.js
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
+import connectDB from "@/app/utils/database";
 
 export async function POST(request) {
     console.log(await request.json())
+    connectDB()
     return NextResponse.json({message: "アイテム作成"})
 }
