3161d15443afab75729035cfff045f6d824759e9
usxc <tinypopopo@gmail.com>
2025-10-30 12:13:55 +0900

    ユーザー登録機能を実装し、POSTメソッドを追加

---
+++ app/api/user/register/route.js
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+import connectDB from "@/app/utils/database";
+import { UserModel } from "@/app/utils/schemaModels";
+
+export async function POST(request) {
+    const reqBody = await request.json()
+
+    try{
+        await connectDB()
+        await UserModel.create(reqBody)
+        return NextResponse.json({message: "ユーザー登録成功"})
+    }catch{
+        return NextResponse.json({message: "ユーザー登録失敗"}) 
+    }
+}
