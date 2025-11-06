222813a444fc2c3f8efccd51d115186079d79964
usxc <tinypopopo@gmail.com>
2025-11-06 11:23:18 +0900

    ログイン機能にJWTトークン生成を追加し、成功時にトークンをレスポンスに含めるように修正

--- app/api/user/login/route.js
+++ app/api/user/login/route.js
@@ -1,4 +1,5 @@
 import { NextResponse } from "next/server"
+import { SignJWT } from "jose"
 import connectDB from "@/app/utils/database"
 import { UserModel } from "@/app/utils/schemaModels"
 
@@ -9,7 +10,15 @@ export async function POST(request){
         const savedUserData = await UserModel.findOne({email: reqBody.email})
         if (savedUserData) {
             if (reqBody.password === savedUserData.password) {
-                return NextResponse.json({message: "ログイン成功"})
+                const secretKey = new TextEncoder().encode("next-market-app-book")
+
+                const payload = {
+                    email: reqBody.email
+                }
+
+                const token = await new SignJWT(payload).setProtectedHeader({alg: "HS256"}).setExpirationTime("1d").sign(secretKey)
+                console.log(token)
+                return NextResponse.json({message: "ログイン成功", token: token})
             } else {
                 return NextResponse.json({message: "ログイン失敗：パスワードが間違っています"})
             }
