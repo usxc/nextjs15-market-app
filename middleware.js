d608134b77c03be31e42078a0bfe38e3fcda4aad
usxc <tinypopopo@gmail.com>
2025-11-13 09:51:45 +0900

    特定のAPIエンドポイントに対するマッチャーを定義

--- middleware.js
+++ middleware.js
@@ -4,3 +4,7 @@ export async function middleware() {
     console.log("ミドルウェア")
     return NextResponse.next()
 }
+
+export const config = {
+    matcher: ["/api/item/create", "/api/item/update/:path*", "/api/item/delete/:path*"],
+}
