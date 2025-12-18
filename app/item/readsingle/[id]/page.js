454f9fd2f1a7e89902f794646cdb996ef9331d2b
usxc <tinypopopo@gmail.com>
2025-12-18 12:32:20 +0900

    個別アイテムの取得機能を追加し、ReadSingleItemコンポーネントを非同期に変更

--- app/item/readsingle/[id]/page.js
+++ app/item/readsingle/[id]/page.js
@@ -1,4 +1,11 @@
-const ReadSingleItem = () => {
+const getSingleItem = async(id) => {
+    console.log(id)
+    // const response = await fetch("")
+}
+
+const ReadSingleItem = async(context) => {
+    const params = await context.params
+    getSingleItem(params.id)
     return (
         <h1>個別アイテムページ</h1>
     )
