cccac5eee3f4ae2dde8fdf56593fabb2166bb63f
usxc <tinypopopo@gmail.com>
2025-12-18 10:18:05 +0900

    非同期関数を使用して全アイテムを取得する機能を追加

--- app/page.js
+++ app/page.js
@@ -1,4 +1,13 @@
-const ReadAllItems = () => {
+const getAllItems = async() => {
+    const response = await fetch("http://localhost:3000/api/item/readall", {cache: "no-store"})
+    const jsonData = await response.json()
+    const allItems = jsonData.allItems
+    return allItems
+}
+
+const ReadAllItems = async() => {
+    const allItems = await getAllItems()
+    console.log(allItems)
     return (
         <div>
             <h1 className="h1-style">こんにちは</h1>
