903c23af6d48c366df47101f1e511db4d8732318
usxc <tinypopopo@gmail.com>
2026-01-30 15:27:02 +0900

    全アイテム表示にタイトルを追加し、説明をh4からpタグに変更

--- app/page.js
+++ app/page.js
@@ -12,14 +12,13 @@ const ReadAllItems = async() => {
     const allItems = await getAllItems()
     return (
         <div>
-            <h1 className="h1-style">こんにちは</h1>
             {allItems.map(item => 
                 <Link href="" key={item._id}>
                     <Image src={item.image} width={750} height={500} alt="item-image" priority/>
                     <div>
                         <h2>{item.price}</h2>
                         <h3>{item.title}</h3>
-                        <h4>{item.description.substring(0, 80)}</h4>
+                        <p>{item.description.substring(0, 80)}</p>
                     </div>
                 </Link>
             )}
