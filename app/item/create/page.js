78c3bb57025658f9766ae8b3ee12a5fcf77db055
usxc <tinypopopo@gmail.com>
2026-02-05 11:14:00 +0900

    アイテム作成フォームに状態管理を追加

--- app/item/create/page.js
+++ app/item/create/page.js
@@ -1,14 +1,21 @@
 "use client"
+import { useState } from "react"
 
 const CreateItem = () => {
+
+    const [title, setTitle] = useState("")
+    const [price, setPrice] = useState("")
+    const [image, setImage] = useState("")
+    const [description, setDescription] = useState("")
+
     return (
         <div>
             <h1>アイテム作成</h1>
             <form>
-                <input type="text" name="title" placeholder="アイテム名" required/>
-                <input type="number" name="price" placeholder="価格" required/>
-                <input type="text" name="image" placeholder="画像" required/>
-                <textarea name="description" placeholder="商品説明" required></textarea>
+                <input value={title} onChange={(e) => setTitle(e.target.value)} type="text" name="title" placeholder="アイテム名" required/>
+                <input value={price} onChange={(e) => setPrice(e.target.value)} type="number" name="price" placeholder="価格" required/>
+                <input value={image} onChange={(e) => setImage(e.target.value)} type="text" name="image" placeholder="画像" required/>
+                <textarea value={description} onChange={(e) => setDescription(e.target.value)} name="description" placeholder="商品説明" required></textarea>
                 <button>作成</button>
                 </form>
         </div>
