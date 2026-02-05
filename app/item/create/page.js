777bf92a0ccf79bda2649becc9d4233cd6f23cfb
usxc <tinypopopo@gmail.com>
2026-02-05 10:39:27 +0900

    アイテム作成フォームを追加

--- app/item/create/page.js
+++ app/item/create/page.js
@@ -4,6 +4,13 @@ const CreateItem = () => {
     return (
         <div>
             <h1>アイテム作成</h1>
+            <form>
+                <input type="text" name="title" placeholder="アイテム名" required/>
+                <input type="number" name="price" placeholder="価格" required/>
+                <input type="text" name="image" placeholder="画像" required/>
+                <textarea name="description" placeholder="商品説明" required></textarea>
+                <button>作成</button>
+                </form>
         </div>
     )
 }
